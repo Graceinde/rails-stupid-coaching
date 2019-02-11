ab3632110e09ad3b6ffc6ebc3b34a45887803871
Gracy Soundron <gracesoundron548@gmail.com>
2019-02-11 15:24:56 -0500

    stupid coaching done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,20 @@
 class QuestionsController < ApplicationController
   def ask
-    @question = Question.ask
+    # @question = Question.ask
+    # @question = params[:ask_type]
   end
 
   def answer
-    @question = Question.answer
+    @questions = params[:question]
+
+    # render plain: "Ask a question '#{params[:question]}' reply'#{params[:answer]}'"
+
+    if @questions == 'I am going to work'
+      @answer = 'Great!'
+    elsif @questions.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/questions', to: 'questions#ask'
-  get '/questions', to: 'questions#answer'
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+  # post 'questions', to: 'questions#answer'
 end
